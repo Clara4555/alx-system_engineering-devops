22460ab565827a95dc678b42a4611782a1c8a12d
Clara4555 <favourloyalty8@gmail.com>
2023-10-03 14:50:21 -0700

    tasks done

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,17 +1,6 @@
 #!/usr/bin/env ruby
 
-# Define an array of patterns with increasing 't' characters
-patterns = ["hbttn", "hbtttn", "hbttttn", "hbtttttn"]
-
-# Get the argument from the command line
-if ARGV.empty?
-  puts "Please provide an argument."
-else
-  input = ARGV[0]
-
-  # Loop through each pattern and scan for matches
-  patterns.each do |pattern|
-    matches = input.scan(/#{pattern}/)
-    puts "#{pattern}: #{matches.join}" unless matches.empty?
-  end
-end
+puts ARGV[0].scan(/hbttn/).join
+puts ARGV[0].scan(/hbtttn/).join
+puts ARGV[0].scan(/hbttttn/).join
+puts ARGV[0].scan(/hbtttttn/).join
