5f97551cfc60a76fecf503a72f4e9d6b35e36abe
Clara4555 <favourloyalty8@gmail.com>
2023-10-03 14:08:13 -0700

    task1

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,18 +1,3 @@
 #!/usr/bin/env ruby
 
-# Check if the argument contains the word "School"
-def match_school(arg)
-  # Define the regular expression to match "School" (case-insensitive)
-  regex = /School/i
-
-  if arg =~ regex
-    puts arg
-  end
-end
-
-if ARGV.empty?
-  puts "Please provide an argument."
-else
-  argn = ARGV[0]
-  match_school(arg)
-end
+puts ARGV[0].scan(/school/i).join
